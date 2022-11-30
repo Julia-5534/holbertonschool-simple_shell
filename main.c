e185f9210a42e6d3521dc820327a68325ad52b71
Chris Stamper <cstampok@gmail.com>
2022-11-30 09:46:43 -0600

    fix betty indents and spaces in main

--- main.c
+++ main.c
@@ -9,61 +9,60 @@
  */
 int main(int argc, char *argv[], char *envp[])
 {
-    pid_t child_pid;
+	pid_t child_pid;
 	int stat1, i = 0;
-    ssize_t eRet = 0;
+	ssize_t eRet = 0;
 	char *line = NULL, *moneySign;
-    char **command;
+	char **command;
 	size_t n = 0;
-    char *thePath = NULL, **pathArr;
+	char *thePath = NULL, **pathArr;
 
-    moneySign = "$ ";
-    pathArr = path_locate(envp);
-    if (argc || argv[0] || thePath)
-    {
-        /* placeholder */
-    }
-	while (1)
-    {
-        write(STDOUT_FILENO, moneySign, 2);
-        eRet = getline(&line, &n, stdin);
-        if (eRet == -1)
-        {
-            break;
-        }
-        command = get_input(line);
-        thePath = check_paths(pathArr, command[0]);
-        if (_strcmp("exit", command[0]) == 0)
-        {
-            break;
-        }
-        if (_strcmp("env", command[0]) == 0)
-        {
-            for (i = 0; envp[i]; i++)
-            {
-                printf("%s\n", envp[i]);
-            }
-        }
-        if (!(thePath))
-        {
-            printf("./hsh: %s not found\n", command[0]);
-            continue;
-        }
-        child_pid = fork();
-        if (child_pid == 0)
-        {
-            execve(thePath, command, envp);
-        }
-        else
-        {
-            waitpid(child_pid, &stat1, WUNTRACED);
-        }
-
+	moneySign = "$ ";
+	pathArr = path_locate(envp);
+	if (argc || argv[0] || thePath)
+	{
+		/* placeholder */
 	}
-    free_tokens(command);
-    free(thePath);
-    free(command);
-    free_path(pathArr);
-    write(STDOUT_FILENO, "\n", 1);
-    return (0);
+	while (1)
+	{
+		write(STDOUT_FILENO, moneySign, 2);
+		eRet = getline(&line, &n, stdin);
+		if (eRet == -1)
+		{
+			break;
+		}
+		command = get_input(line);
+		thePath = check_paths(pathArr, command[0]);
+		if (_strcmp("exit", command[0]) == 0)
+		{
+			break;
+		}
+		if (_strcmp("env", command[0]) == 0)
+		{
+			for (i = 0; envp[i]; i++)
+			{
+				printf("%s\n", envp[i]);
+			}
+		}
+		if (!(thePath))
+		{
+			printf("./hsh: %s not found\n", command[0]);
+			continue;
+		}
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			execve(thePath, command, envp);
+		}
+		else
+		{
+			waitpid(child_pid, &stat1, WUNTRACED);
+		}
+	}
+	free_tokens(command);
+	free(thePath);
+	free(command);
+	free_path(pathArr);
+	write(STDOUT_FILENO, "\n", 1);
+	return (0);
 }
